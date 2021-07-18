f3a73be15c799f6bca06c2bc0a774decd16ce30b
Cody Wilson <cody@codifynw.com>
2021-07-17 22:17:04 -0700

    1

--- js/easy/1.js
+++ js/easy/1.js
@@ -1,16 +1,48 @@
-function twoNumberSum(array, targetSum) {
-  const nums = {};
+// function twoNumberSum(array, targetSum) {
+//   const nums = {};
 
-  for (const num of array) {
-    const potentialMatch = targetSum - num;
-    if (potentialMatch in nums) {
-      return [potentialMatch, num];
-    } else {
-      nums[num] = true;
-    }
-  }
+//   for (const num of array) {
+//     const potentialMatch = targetSum - num;
+//     if (potentialMatch in nums) {
+//       return [potentialMatch, num];
+//     } else {
+//       nums[num] = true;
+//     }
+//   }
 
-  return [];
-}
+//   return [];
+// }
+
+// MISTAKE 7/16
+// THE ERROR HERE IS USING *IN* INSTEAD OF *OF*
+// TO LOOP THROUGH THE ARRAY.
+// function twoNumberSum(array, targetSum) {
+//   let checkedHash = {};
+
+//   for (const number in array) {
+//     const neededNumber = targetSum - number;
+//     if (neededNumber in checkedHash) {
+//       return [neededNumber, number];
+//     } else {
+//       checkedHash[number] = true;
+//     }
+//   }
+//   return [];
+// }
+
+// 7/17
+// function twoNumberSum(array, targetSum) {
+//   const nums = {};
+
+//   for (number of array) {
+//     difference = targetSum - number;
+//     if (difference in nums) {
+//       return [number, difference];
+//     } else {
+//       nums[number] = true;
+//     }
+//   }
+//   return [];
+// }
 
 exports.twoNumberSum = twoNumberSum;
