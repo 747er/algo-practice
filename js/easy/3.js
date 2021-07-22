aa3915f977f20c38b56d2158c6002cbceeff60fb
Cody Wilson <cody@codifynw.com>
2021-07-22 08:59:14 -0700

    3 revisit

--- js/easy/3.js
+++ js/easy/3.js
@@ -1,3 +1,25 @@
+// function sortedSquaredArray(array) {
+//   // Write your code here.
+//   const sortedSquares = new Array(array.length).fill(0);
+//   let smallerValueIdx = 0;
+//   let largerValueIdx = array.length - 1;
+
+//   for (let idx = array.length - 1; idx >= 0; idx--) {
+//     const smallerValue = array[smallerValueIdx];
+//     const largerValue = array[largerValueIdx];
+
+//     if (Math.abs(smallerValue) > Math.abs(largerValue)) {
+//       sortedSquares[idx] = smallerValue * smallerValue;
+//       smallerValueIdx++;
+//     } else {
+//       sortedSquares[idx] = largerValue * largerValue;
+//       largerValueIdx--;
+//     }
+//   }
+
+//   return sortedSquares;
+// }
+
 function sortedSquaredArray(array) {
   // Write your code here.
   const sortedSquares = new Array(array.length).fill(0);
@@ -5,15 +27,19 @@ function sortedSquaredArray(array) {
   let largerValueIdx = array.length - 1;
 
   for (let idx = array.length - 1; idx >= 0; idx--) {
-    const smallerValue = array[smallerValueIdx];
-    const largerValue = array[largerValueIdx];
+    let largerValue = array[largerValueIdx] * array[largerValueIdx];
+    let smallerValue = array[smallerValueIdx] * array[smallerValueIdx];
 
-    if (Math.abs(smallerValue) > Math.abs(largerValue)) {
-      sortedSquares[idx] = smallerValue * smallerValue;
-      smallerValueIdx++;
-    } else {
-      sortedSquares[idx] = largerValue * largerValue;
+    if (smallerValueIdx === largerValueIdx) {
+      sortedSquares[idx] = largerValue;
+    }
+    if (largerValue > smallerValue) {
+      sortedSquares[idx] = largerValue;
       largerValueIdx--;
+    }
+    if (largerValue < smallerValue) {
+      sortedSquares[idx] = smallerValue;
+      smallerValueIdx++;
     }
   }
 
