4be03537944f514ed18d500b6025a9e7edf6e8d7
Cody Wilson <cody@codifynw.com>
2021-08-02 08:59:48 -0700

    12 hard

--- js/hard/12.js
+++ js/hard/12.js
@@ -1,5 +1,24 @@
 function minNumberOfJumps(array) {
   // Write your code here.
+  let jumps = 1;
+  let position = 0;
+
+  let findNextIndex = function (position) {
+    let nextPosition = array[position] + position - 1;
+    for (let j = position + 1; j <= nextPosition; j++) {
+      if (array[j] > nextPosition) {
+        nextPosition = j;
+      }
+    }
+    return nextPosition + position;
+  };
+
+  while (position < array.length) {
+    jumps++;
+    position = findNextIndex(position);
+  }
+
+  return jumps;
 }
 
 // Do not edit the line below.
--- js/hard/12.test.js
+++ js/hard/12.test.js
@@ -5,7 +5,7 @@
 const program = require("./12");
 const chai = require("chai");
 
-it("Test Case #1", function () {
+it("Hard 12, Test Case #1", function () {
   chai
     .expect(program.minNumberOfJumps([3, 4, 2, 1, 2, 3, 7, 1, 1, 1, 3]))
     .to.deep.equal(4);
