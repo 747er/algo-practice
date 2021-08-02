2a23a3fbaa5deff225bb2118786222184e0bf18c
Cody Wilson <cody@codifynw.com>
2021-08-01 23:55:50 -0700

    hard 12

---
+++ js/hard/12.js
@@ -0,0 +1,6 @@
+function minNumberOfJumps(array) {
+  // Write your code here.
+}
+
+// Do not edit the line below.
+exports.minNumberOfJumps = minNumberOfJumps;
---
+++ js/hard/12.test.js
@@ -0,0 +1,12 @@
+// This file is initialized with a code version of this
+// question's sample test case. Feel free to add, edit,
+// or remove test cases in this file as you see fit!
+
+const program = require("./12");
+const chai = require("chai");
+
+it("Test Case #1", function () {
+  chai
+    .expect(program.minNumberOfJumps([3, 4, 2, 1, 2, 3, 7, 1, 1, 1, 3]))
+    .to.deep.equal(4);
+});
