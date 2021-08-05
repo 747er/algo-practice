955d1c195e0a20f4e06205b981f6a51222162689
Cody Wilson <cody@codifynw.com>
2021-08-04 23:36:12 -0700

    o(1) for hard 13

--- js/hard/13.js
+++ js/hard/13.js
@@ -1,25 +1,50 @@
+// function waterArea(heights) {
+//   // Write your code here.
+//   let maxes = new Array(heights.length).fill(0);
+
+//   let leftMax = 0;
+//   for (let i = 0; i < heights.length; i++) {
+//     const height = heights[i];
+//     maxes[i] = leftMax;
+//     leftMax = Math.max(leftMax, height);
+//   }
+//   let rightMax = 0;
+//   for (let i = heights.length - 1; i >= 0; i--) {
+//     const height = heights[i];
+//     const minHeight = Math.min(rightMax, maxes[i]);
+//     if (height < minHeight) {
+//       maxes[i] = minHeight - height;
+//     } else {
+//       maxes[i] = 0;
+//     }
+//     rightMax = Math.max(rightMax, height);
+//   }
+//   return maxes.reduce((a, b) => a + b, 0);
+// }
+
 function waterArea(heights) {
   // Write your code here.
-  let maxes = new Array(heights.length).fill(0);
+  if (heights.length === 0) return 0;
 
-  let leftMax = 0;
-  for (let i = 0; i < heights.length; i++) {
-    const height = heights[i];
-    maxes[i] = leftMax;
-    leftMax = Math.max(leftMax, height);
-  }
-  let rightMax = 0;
-  for (let i = heights.length - 1; i >= 0; i--) {
-    const height = heights[i];
-    const minHeight = Math.min(rightMax, maxes[i]);
-    if (height < minHeight) {
-      maxes[i] = minHeight - height;
+  let leftIdx = 0;
+  let rightIdx = heights.length - 1;
+  let leftMax = heights[leftIdx];
+  let rightMax = heights[rightIdx];
+  let surfaceArea = 0;
+
+  while (leftIdx < rightIdx) {
+    if (heights[leftIdx] < heights[rightIdx]) {
+      leftIdx++;
+      leftMax = Math.max(leftMax, heights[leftIdx]);
+      surfaceArea += leftMax - heights[leftIdx];
     } else {
-      maxes[i] = 0;
+      rightIdx--;
+      rightMax = Math.max(rightMax, heights[rightIdx]);
+      surfaceArea += rightMax - heights[rightIdx];
     }
-    rightMax = Math.max(rightMax, height);
   }
-  return maxes.reduce((a, b) => a + b, 0);
+
+  return surfaceArea;
 }
 
 // Do not edit the line below.
