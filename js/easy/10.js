e5f333d582e01bdc5a68a337e1f272689c9c9b2b
Cody Wilson <cody@codifynw.com>
2021-08-07 23:47:31 -0700

    easy 10 revisit, one liner

--- js/easy/10.js
+++ js/easy/10.js
@@ -1,15 +1,24 @@
 function minimumWaitingTime(queries) {
   queries.sort((a, b) => a - b);
-
-  let waitingTime = 0;
   let totalWaitingTime = 0;
-  for (const duration of queries) {
-    totalWaitingTime += waitingTime;
-    waitingTime += duration;
+
+  for (let i = 1; i < queries.length; i++) {
+    totalWaitingTime += queries[i - 1] * (queries.length - i);
   }
 
   return totalWaitingTime;
 }
+
+// queries.sort((a, b) => a - b);
+
+// let waitingTime = 0;
+// let totalWaitingTime = 0;
+// for (const duration of queries) {
+//   totalWaitingTime += waitingTime;
+//   waitingTime += duration;
+// }
+
+// return totalWaitingTime;
 
 // Do not edit the line below.
 exports.minimumWaitingTime = minimumWaitingTime;
