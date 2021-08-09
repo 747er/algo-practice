acd5739984865e020e0c4bdaf98b56dcbeb32342
Cody Wilson <cody@codifynw.com>
2021-08-08 22:32:01 -0700

    optimized getNthFib

--- js/easy/14.js
+++ js/easy/14.js
@@ -1,15 +1,27 @@
+// function getNthFib(n) {
+//   // Write your code here.
+//   let fib = [0, 1];
+
+//   for (let i = 2; i < n + 1; i++) {
+//     fib.push(fib[i - 1] + fib[i - 2]);
+//   }
+//   return fib[n - 1];
+// }
+
+// // Do not edit the line below.
+// exports.getNthFib = getNthFib;
+
 function getNthFib(n) {
   // Write your code here.
-  let fib = [0, 1];
-
-  for (let i = 2; i < n + 1; i++) {
-    console.log(fib[i - 1]);
-    console.log(fib[i - 2]);
-    fib.push(fib[i - 1] + fib[i - 2]);
+  const lastTwo = [0, 1];
+  let counter = 3;
+  while (counter <= n) {
+    const nextFib = lastTwo[0] + lastTwo[1];
+    lastTwo[0] = lastTwo[1];
+    lastTwo[1] = nextFib;
+    counter++;
   }
-
-  console.log(fib);
-  return fib[n - 1];
+  return n > 1 ? lastTwo[1] : lastTwo[0];
 }
 
 // Do not edit the line below.
