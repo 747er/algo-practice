9878d15ba0589054c0a98a1bfd7d4717a9a58cb3
Cody Wilson <cody@codifynw.com>
2021-08-28 22:06:02 -0700

    kadane

---
+++ js/famous/kadanes.js
@@ -0,0 +1,14 @@
+function kadanesAlgorithm(array) {
+  let maxEndingHere = array[0];
+  let maxSoFar = array[0];
+
+  for (let i = 1; i < array.length; i++) {
+    const num = array[i];
+    maxEndingHere = Math.max(num, maxEndingHere + num);
+    maxSoFar = Math.max(maxSoFar, maxEndingHere);
+  }
+  return maxSoFar;
+}
+
+// Do not edit the line below.
+exports.kadanesAlgorithm = kadanesAlgorithm;
---
+++ js/famous/kadanes.test.js
@@ -0,0 +1,16 @@
+// This file is initialized with a code version of this
+// question's sample test case. Feel free to add, edit,
+// or remove test cases in this file as you see fit!
+
+const program = require("./program");
+const chai = require("chai");
+
+it("Test Case #1", function () {
+  chai
+    .expect(
+      program.kadanesAlgorithm([
+        3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4,
+      ])
+    )
+    .to.deep.equal(19);
+});
