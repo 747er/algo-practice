f8781beaf6ef268767de0a0ed69f5ea56dca39c2
Cody Wilson <cody@codifynw.com>
2021-10-22 22:32:25 -0700

    pal #2

--- js/strings/longestPal.js
+++ js/strings/longestPal.js
@@ -1,11 +1,10 @@
 // Given a string s, return the longest palindromic substring in s.
 
-const findLongestPalindrome = (str, i, j) => {
+const checkPalindrome = function (str, i, j) {
   while (i >= 0 && j < str.length && str[i] === str[j]) {
     i -= 1;
     j += 1;
   }
-  // slice the qualified substring from the second last iteration
   return str.slice(i + 1, j);
 };
 
@@ -13,22 +12,49 @@ var longestPalindrome = function (s) {
   let longest = "";
 
   for (let i = 0; i < s.length; i++) {
-    // palindrome can center around 1 or 2 letters
-    const current1 = findLongestPalindrome(s, i, i);
-    const current2 = findLongestPalindrome(s, i, i + 1);
+    const center1 = checkPalindrome(s, i, i);
+    const center2 = checkPalindrome(s, i, i + 1);
 
-    console.log("current1: ", current1);
-    console.log("current2: ", current2);
+    if (center1.length > longest.length) {
+      longest = center1;
+    }
 
-    const longerPalindrome =
-      current1.length > current2.length ? current1 : current2;
-
-    if (longerPalindrome.length > longest.length) {
-      longest = longerPalindrome;
+    if (center2.length > longest.length) {
+      longest = center2;
     }
   }
+
   return longest;
 };
+
+console.log(longestPalindrome("daadracecarmamapop"));
+
+// const findLongestPalindrome = (str, i, j) => {
+//   while (i >= 0 && j < str.length && str[i] === str[j]) {
+//     i -= 1;
+//     j += 1;
+//   }
+//   // slice the qualified substring from the second last iteration
+//   return str.slice(i + 1, j);
+// };
+
+// var longestPalindrome = function (s) {
+//   let longest = "";
+
+//   for (let i = 0; i < s.length; i++) {
+//     // palindrome can center around 1 or 2 letters
+//     const current1 = findLongestPalindrome(s, i, i);
+//     const current2 = findLongestPalindrome(s, i, i + 1);
+
+//     const longerPalindrome =
+//       current1.length > current2.length ? current1 : current2;
+
+//     if (longerPalindrome.length > longest.length) {
+//       longest = longerPalindrome;
+//     }
+//   }
+//   return longest;
+// };
 
 // console.log(longestPalindrome("abbadadracecar"));
 
