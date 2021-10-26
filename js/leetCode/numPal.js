3d79d9d572b5168461e201f841f0f39587ddf13e
Cody Wilson <cody@codifynw.com>
2021-10-25 17:24:00 -0700

    more solutions for numPal

--- js/leetCode/numPal.js
+++ js/leetCode/numPal.js
@@ -2,18 +2,54 @@
  * @param {number} x
  * @return {boolean}
  */
+// var isPalindrome = function (x) {
+//   let str = x.toString();
+//   let lIdx = str.length - 1;
+//   let rIdx = 0;
+
+//   while (rIdx < str.length && lIdx >= -1 && str[lIdx] === str[rIdx]) {
+//     lIdx--;
+//     rIdx++;
+//   }
+
+//   return rIdx === str.length ? true : false;
+// };
+
+// Upgraded First Solution
 var isPalindrome = function (x) {
   let str = x.toString();
-  let lIdx = str.length - 1;
-  let rIdx = 0;
+  let lIdx = 0;
+  let rIdx = str.length - 1;
 
-  while (rIdx < str.length && lIdx >= -1 && str[lIdx] === str[rIdx]) {
-    lIdx--;
-    rIdx++;
+  while (lIdx < rIdx) {
+    if (str[lIdx++] !== str[rIdx--]) return false;
   }
 
-  return rIdx === str.length ? true : false;
+  return true;
 };
+
+// Alt Solution 1
+// var isPalindrome = function (x) {
+//   const arr = String(x).split("");
+
+//   while (arr.length > 1) {
+//     if (arr.shift() !== arr.pop()) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// };
+
+// Alt Solution 2
+// var isPalindrome = function (x) {
+//   if (x < 0) return false;
+
+//   // reverse the string representation of x
+//   const reverse = `${x}`.split("").reverse().join("");
+//   // compare the value regardless of types
+//   return x == reverse;
+// };
 
 console.log(isPalindrome(123));
 console.log(isPalindrome(123321));
