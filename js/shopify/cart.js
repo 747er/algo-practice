c6f6cd65f2bcd63ac6215de3cf3112a52c0637df
Cody Wilson <cody@codifynw.com>
2021-11-03 17:49:44 -0700

    updated discount and testing in shop cart

--- js/shopify/cart.js
+++ js/shopify/cart.js
@@ -1,24 +1,39 @@
 let cart = {
   currency: "USD",
+
   discounts: [
     {
       title: "Welcome10",
       amount: 10,
       type: "fixed",
     },
+    {
+      title: "BOGOHO",
+      amount: 50,
+      itemId: 456,
+      type: "percent",
+    },
   ],
+
   items: [
     {
       title: "Product One",
+      id: 22,
       weight: 12,
       price: 1200,
       product_discount: 200,
     },
+    {
+      title: "Product One",
+      id: 456,
+      weight: 12,
+      price: 3000,
+      product_discount: 100,
+    },
   ],
-  original_price() {
-    return 12;
-  },
-  total_price() {
+
+  //   TOTAL PRICE WITH NO DISCOUNTS
+  original_total_price() {
     // MOST BASIC
     // const self = this;
     // let total = 0;
@@ -26,21 +41,39 @@ let cart = {
     //   total += self.items[i].price;
     // }
 
-    // FILTER
-
     // REDUCE
-    const sum = this.items.reduce((accumulator, currentValue) => {
-      console.log(currentValue);
-      return accumulator + currentValue.price;
+    const sum = this.items.reduce((total, currentItem) => {
+      return total + currentItem.price;
     }, 0);
     return sum;
-
-    // MAP
-
-    // return total;
   },
-  total_discount() {},
+
+  //   TOTAL PRICE AFTER APPLYING DISCOUNTS
+  total_price() {
+    return this.original_total_price() - this.total_discount();
+  },
+
+  //   SUM OF ALL DISCOUNTS APPLIED
+  total_discount() {
+    const lineItemDiscount = this.__line_item_discounts();
+
+    return lineItemDiscount;
+  },
+
+  __line_item_discounts() {
+    const discount = this.items.reduce((total, currItem) => {
+      return (total +=
+        currItem.product_discount > 0 ? currItem.product_discount : 0);
+    }, 0);
+    return discount;
+  },
 };
 
-console.log(cart.total_price());
-// console.assert(cart.original_price() === 11);
+// console.log(cart.original_total_price());
+// console.log(cart.total_price());
+
+const originalTotalPrice = cart.original_total_price();
+console.assert(
+  cart.original_total_price() === 4200,
+  `Original Total Price of ${originalTotalPrice} does not equal 4200`
+);
