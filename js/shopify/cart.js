4066071abde6147c86b5e29165d4f0a10018db5a
Cody Wilson <cody@codifynw.com>
2021-11-04 19:10:33 -0700

    working cart w/ original, discount, and total

--- js/shopify/cart.js
+++ js/shopify/cart.js
@@ -4,13 +4,12 @@ let cart = {
   discounts: [
     {
       title: "Welcome10",
-      amount: 10,
+      amount: 1000,
       type: "fixed",
     },
     {
       title: "BOGOHO",
-      amount: 50,
-      itemId: 456,
+      amount: 10,
       type: "percent",
     },
   ],
@@ -20,28 +19,20 @@ let cart = {
       title: "Product One",
       id: 22,
       weight: 12,
-      price: 1200,
+      price: 2200,
       product_discount: 200,
     },
     {
       title: "Product One",
       id: 456,
       weight: 12,
-      price: 3000,
+      price: 3100,
       product_discount: 100,
     },
   ],
 
   //   TOTAL PRICE WITH NO DISCOUNTS
   original_total_price() {
-    // MOST BASIC
-    // const self = this;
-    // let total = 0;
-    // for (let i = 0; i < self.items.length; i++) {
-    //   total += self.items[i].price;
-    // }
-
-    // REDUCE
     const sum = this.items.reduce((total, currentItem) => {
       return total + currentItem.price;
     }, 0);
@@ -55,12 +46,36 @@ let cart = {
 
   //   SUM OF ALL DISCOUNTS APPLIED
   total_discount() {
-    const lineItemDiscount = this.__line_item_discounts();
+    let discounts = {
+      fixed: 0,
+      percent: 0,
+      lineItem: this.__calculate_line_item_discounts(),
+    };
 
-    return lineItemDiscount;
+    for (let i = 0; i < this.discounts.length; i++) {
+      switch (this.discounts[i].type) {
+        case "fixed":
+          discounts["fixed"] += this.discounts[i].amount;
+          break;
+        case "percent":
+          discounts["percent"] += this.discounts[i].amount;
+          break;
+      }
+    }
+
+    percentDiscount =
+      (this.original_total_price() -
+        discounts["lineItem"] -
+        discounts["fixed"]) *
+      (discounts["percent"] / 100);
+
+    const discount =
+      discounts["fixed"] + discounts["lineItem"] + percentDiscount;
+
+    return discount;
   },
 
-  __line_item_discounts() {
+  __calculate_line_item_discounts() {
     const discount = this.items.reduce((total, currItem) => {
       return (total +=
         currItem.product_discount > 0 ? currItem.product_discount : 0);
@@ -69,11 +84,24 @@ let cart = {
   },
 };
 
-// console.log(cart.original_total_price());
-// console.log(cart.total_price());
-
 const originalTotalPrice = cart.original_total_price();
+console.log("originalTotalPrice: ", originalTotalPrice);
 console.assert(
-  cart.original_total_price() === 4200,
-  `Original Total Price of ${originalTotalPrice} does not equal 4200`
+  cart.original_total_price() === 5300,
+  `Error: Original Total Price of ${originalTotalPrice} does not equal 5300`
+);
+
+const totalDiscounts = cart.total_discount();
+console.log("totalDiscounts: ", totalDiscounts);
+
+console.assert(
+  cart.total_discount() === 1700,
+  `Error: Total Discount of ${totalDiscounts} does not equal 1700`
+);
+
+const totalPrice = cart.total_price();
+console.log("total price: ", totalPrice);
+console.assert(
+  cart.total_price() === 3600,
+  `Error: Total Discount of ${totalPrice} does not equal 3600`
 );
