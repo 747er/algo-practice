6dfacabc5c55fc458acb0b6f85db63019c07bc2d
Cody Wilson <cody@codifynw.com>
2021-12-04 15:31:32 -0800

    bst construction (wo remove)

---
+++ js/binary-tree/bst-construction.js
@@ -0,0 +1,65 @@
+const chai = require("chai");
+
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BST(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BST(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
+
+    return this;
+  }
+
+  contains(value) {
+    console.log(value);
+    if (value < this.value) {
+      if (this.left === null) {
+        return false;
+      } else {
+        return this.left.contains(value);
+      }
+    } else if (value > this.value) {
+      if (this.right === null) {
+        return false;
+      } else {
+        return this.right.contains(value);
+      }
+    } else {
+      return true;
+    }
+  }
+}
+
+const root = new BST(10);
+root.left = new BST(5);
+root.left.left = new BST(2);
+root.left.left.left = new BST(1);
+root.left.right = new BST(5);
+root.right = new BST(15);
+root.right.left = new BST(13);
+root.right.left.right = new BST(14);
+root.right.right = new BST(22);
+
+root.insert(12);
+chai.expect(root.right.left.left.value).to.deep.equal(12);
+
+// root.remove(10);
+chai.expect(root.contains(10)).to.deep.equal(true);
+chai.expect(root.value).to.deep.equal(10);
+
+chai.expect(root.contains(15)).to.deep.equal(true);
