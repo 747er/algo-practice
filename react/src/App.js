54b15fb2b9028bcfc7fa4b02efba7316155bd086
Cody Wilson <cody@codifynw.com>
2021-12-07 13:08:32 -0800

    candycrush

---
+++ react/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Board from "./components/Board";
+
+function App() {
+  return (
+    <div className="board-container">
+      <Board />
+    </div>
+  );
+}
+
+export default App;
