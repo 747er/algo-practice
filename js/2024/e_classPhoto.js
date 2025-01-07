496ab76a51527f7258c449417904368f70916b25
codifynw <cody@codifynw.com>
2025-01-06 22:20:28 -0800

    classPhoto2

--- js/2024/e_classPhoto.js
+++ js/2024/e_classPhoto.js
@@ -14,6 +14,25 @@ const classPhotos = (redShirtHeights, blueShirtHeights) => {
       return true;
 }
 
+// Minimize Conditionals
+// const classPhotos = (redShirtHeights, blueShirtHeights) => {
+//   const tallerGroup = redShirtHeights[0] > blueShirtHeights[0] ? 'red' : 'blue';
+//   const shorterGroup = redShirtHeights[0] > blueShirtHeights[0] ? 'blue' : 'red';
+
+//   const heights = {
+//       'red': redShirtHeights.sort((a, b) => a - b),
+//       'blue': blueShirtHeights.sort((a, b) => a - b),
+//   }
+  
+
+
+//   for (let index = 0; index < redShirtHeights.length; index++) {
+//       if(heights[tallerGroup][index] < heights[shorterGroup][index]) return false
+//     }
+    
+//     return true;
+// }
+
 const redShirtHeights = [5, 8, 1, 3, 2];
 const blueShirtHeights = [6, 9, 2, 4, 5];
 
